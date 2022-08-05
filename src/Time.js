b49679da183ae8e2fdb886fa032dc1df37ac9508
kayagoksu <goksukaya92@gmail.com>
2022-08-05 23:21:11 +0300

    update

--- src/Time.js
+++ src/Time.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 const Time = () => {
     return (
-        <div>Time</div>
+        <div>
+
+            <div>
+                <h3>Report for</h3>
+                <h1>Jeremy Robson</h1>
+            </div>
+            <div>
+                <a href="">Daily</a>
+                <a href="">Weekly</a>
+                <a href="">Monthly</a>
+            </div>
+
+
+        </div>
     );
 };
 
